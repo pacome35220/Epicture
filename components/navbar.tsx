e84db627c6e3aa2dd303f4807e9e3182174e5f8f
Marco GAUTIER <marco.gautier@epitech.eu>
2019-10-20 04:33:52 +0200

    refactor(code): remove deprecated code from navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -11,28 +11,4 @@ const NavbarTab = createMaterialBottomTabNavigator({
     UserProfileTab: { screen: UserProfile } ,
 });
 
-class Navbar extends React.Component {
-    render() {
-        return (
-            <View style= {styles.navbar}>
-                <View style={{flex: 1, backgroundColor: 'blue'}}></View>
-                <View style={{flex: 1, backgroundColor: 'green'}}></View>
-                <View style={{flex: 1, backgroundColor: 'yellow'}}></View>
-                <View style={{flex: 1, backgroundColor: 'black'}}></View>
-            </View>
-        )
-    }
-}
-
-const styles = StyleSheet.create({
-    navbar: {
-        height: 100,
-        flexDirection: 'row',
-        backgroundColor: 'red'
-    },
-    item: {
-
-    }
-})
-
 export default createAppContainer(NavbarTab)
