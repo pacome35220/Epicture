8610078a486d6235efebfee688eee6a9b3f6fe82
Marco GAUTIER <marco.gautier@epitech.eu>
2019-10-22 15:21:00 +0200

    feat(gallery): show an image from imgur but only if it's a .jpg or .png

--- common/ImgurApi.ts
+++ common/ImgurApi.ts
@@ -1,5 +1,10 @@
+import { AxiosRequestConfig } from 'axios'
+import axios from 'axios'
+import User from './User'
+
 export class ImgurApi {
     private static instance: ImgurApi;
+    private static axios = axios.create()
 
     private constructor() {
         console.log('constructor')
@@ -7,6 +12,13 @@ export class ImgurApi {
 
     getUserProfilePic() {
         return (require('../assets/example_profile.png'))
+    }
+
+    getImageGallery() {
+        let options = {
+            headers: {'Authorization': 'Client-ID' + require('../credentials.json').clientId}
+        }
+        return axios.get('https://api.imgur.com/3/gallery/hot/viral/day/1?showViral=true&mature=false&album_previews=false', options).then((response) => {return response.data.data[5].images[0].link})
     }
 
     static getInstance(): ImgurApi {
--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,15 +1,49 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
+
+import { ImgurApi } from '../common/ImgurApi'
+import { NullConsole } from '@jest/console';
+
+const galleryUrl = 'https://api.imgur.com/3/gallery/hot/viral/day/1?showViral=true&mature=false&album_previews=false'
 
 class Gallery extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            images: [],
+            image: '',
+            imageIsLoading: true
+        }
+    }
+
+    componentDidMount() {
+        this.fetchGalleryImages()
+    }
+
+    private async fetchGalleryImages() {
+        ImgurApi.getInstance().getImageGallery().then((images) => {
+            this.setState({image: images})
+            this.setState({imageIsLoading: false})
+        })
+    }
+
     render() {
-        return (
-            <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-                <Text>
-                    This is gallery tab
-                </Text>
-            </View>
-        )
+        if (this.state.imageIsLoading == false) {
+            console.log(this.state.image)
+            return (
+                <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+                    <Image
+                        style={{width: Dimensions.get('window').width, height: Dimensions.get('window').width}}
+                        source={{uri: this.state.image}}
+                    >
+                    </Image>
+                </View>
+            )
+        } else {
+            return (
+                <Text>BITE</Text>
+            )
+        }
     }
 }
 
